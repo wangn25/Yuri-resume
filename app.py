5d7c14de57ca1afa65ee807610b37608eca614fd
wangn25 <45310885+wangn25@users.noreply.github.com>
2024-03-18 23:09:22 +0800

    Update app.py

--- app.py
+++ app.py
@@ -29,7 +29,7 @@ Strong analytical skill and good at MySQL, PowerQuery, PowerPivot, Automate& Pow
 EMAIL = "swat_2046@hotmail.com"
 SOCIAL_MEDIA = {
    
-    "**🌟Need GPT support🌟**": "https://yuri-knowledgegpt.streamlit.app/",
+    "****🌟Need GPT support?🌟****": "https://yuri-knowledgegpt.streamlit.app/",
     "GitHub": "https://github.com",
    
 }
